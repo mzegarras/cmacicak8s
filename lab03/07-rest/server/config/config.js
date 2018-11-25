62e08df85f0ab8ed9ffd073382a61700d6b032a9
Manuel <mzegarra@gmail.com>
2018-11-25 06:34:46 -0500

    clomplex service

---
+++ lab03/07-rest/server/config/config.js
@@ -0,0 +1,9 @@
+
+///=========================
+///PORT
+///=========================
+
+process.env.PORT = process.env.PORT || 3000;
+process.env.MONGO_USER = process.env.MONGO_USER || "mongouser";
+process.env.MONGO_PWD = process.env.MONGO_PWD || "mongopwd";
+process.env.MONGO_SERVER = process.env.MONGO_SERVER || "mongosrv";
